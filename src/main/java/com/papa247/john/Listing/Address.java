2ef0e8684fe27e38298013db4386baa1a9733b2b
Clmeyers-Dev <70337853+Clmeyers-Dev@users.noreply.github.com>
2020-10-27 04:06:37 -0400

    Finished Address/Listing and Created Room
    Room is inside of the Address file as it's own class

--- src/main/java/com/papa247/john/Listing/Address.java
+++ src/main/java/com/papa247/john/Listing/Address.java
@@ -4,25 +4,57 @@
 */
 
 package com.papa247.john.Listing;
-
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import com.papa247.john.Support.Coordinate;
 import com.papa247.john.Support.Review;
 import com.papa247.john.User.User;
 
 public class Address {
-
-    public Listing[] listings;
-    public String appartmentNumber;
-    public User owner;
+    JSONParser parser = new JSONParser();
+    public String name = "Address";
     public String streetAddress;
     public String city;
+    public String state;
+    public String postalCode;
+    public String country;
+    public Coordinate longitude;
+    public Coordinate latitude;
+    public Review[] reviews;
+    public Listing[] listings;
+    public User[] managers;
     public int id;
     public Coordinate cords;
-    public String name = "Address";
+   
+    
 
     public Address(JSONObject jsonObject) {
-        // TODO Auto-generated constructor stub
+        if (jsonObject.has("Name"))
+            name = (String)jsonObject.get("Name");
+        if (jsonObject.has("streetAddress"))
+            streetAddress = (String)jsonObject.get("streetAddress");
+        if (jsonObject.has("city"))
+            city = (String)jsonObject.get("city");
+        if (jsonObject.has("state"))
+            state = (String)jsonObject.get("state");
+        if (jsonObject.has("postalCode"))
+            postalCode = (String)jsonObject.get("postalCode");
+        if (jsonObject.has("country"))
+            country= (String)jsonObject.get("country");
+        if (jsonObject.has("longitude"))
+            longitude = (Coordinate)jsonObject.get("longitude"); 
+        if (jsonObject.has("Latitude"))
+            Latitude = (Coordinate)jsonObject.get("Latitude"); 
+       
+        if(jsonObject.has("listings"))
+           listings = jsonObject.getJSONArray("listings");
+       
+       if (jsonObject.has("managers"))
+           managers = jsonObject.getJSONArray("managers");
     }
 
     /**
@@ -31,13 +63,110 @@ public class Address {
      * @param listing   The listing... to add...
      */
     public void addListing(Listing listing) {
+        for(int i  = 0; i<listings.length;i++) {
+            if(listings[i]==null) {
+                listings[i] = listing;
+            }
+        }
     }
-
+    public boolean removeListing(Listing listing) {
+        for(int i  = 0; i<listings.length;i++) {
+            if(listings[i]!=null&&listings[i].equals(listing)) {
+                listings[i] = null;
+                return true;
+            }else {
+                return false;
+            }
+        }
+    }
+    public boolean removeManager(User manager) {
+        for(int i = 0;i<managers.length;i++) {
+            if(managers[i]!=null&&managers[i].equals(manager)) {
+                managers[i] = null;
+                return true;
+            }else {
+                return false;
+            }
+        }
+    }
+    public boolean removeReview(Review review) {
+        for(int i = 0; i<reviews.length;i++) {
+            if(reviews[i]!=null&&revies[i].equals(review)) {
+                reviews[i] = null;
+                return true;
+            }else {
+                return false;
+            }
+        }
+           
+    }
+    public boolean addReview(Review review) {
+        for(int i = 0;i<reviews.length;i++) {
+            if(reviews[i]==null) {
+                reviews[i] = review;
+                return true;
+            }else {
+                return false;
+            }
+        }
+    }
+    
     public JSONObject toJSON() {
-        return null;
-    }
+        JSONObject jo = new JSONObject();
+        
+        jo.put("name", name);
+        jo.put("streetAddress", streetAddress);
+        jo.put("city", city);
+        jo.put("state", state);
+        jo.put("postalCode", postalCode);
+        jo.put("country", country);
+        jo.put("longitude", longitude);
+        jo.put("latitude", latitude);
+        
+        
+        JSONArray ja = new JSONArray();
+        for (Listing listing : listings)
+            ja.put(listing.id);
+        jo.put("listings", ja);
+        
+        ja = new JSONArray();
+        for (Review review : reviews)
+            ja.put(reviews.target);
+        jo.put("reviews", ja);
+        
+        ja = new JSONArray();
+        for (User users : managers)
+            ja.put(managers.userName);
+        jo.put("managers", ja);
+        
+        return jo;    }
 
     public void addReviewOf(Review review) {
+        for(int i  = 0; i<listings.length;i++) {
+            if(reviews[i]==null) {
+                reviews[i] = review;
+            }
+        }
     }
 
+}
+public class Room {
+    public RoomType type;
+    public double size;
+    public FloorType floor;
+    public Extensions[]  extensions= new Extensions[];
+    public Appliances[]  appliances= new Appliances[];
+    public Fixtures[]  fixtures= new Fixtures[];
+    public Furniture[]  furniture= new Furniture[];
+    public int windows;
+    public String name;
+    public Room(JSONObject room) {
+        
+    }
+    public void reset() {
+        
+    }
+    public JSONObject toJSON() {
+        
+    }
 }
--- src/main/java/com/papa247/john/Listing/Listing.java
+++ src/main/java/com/papa247/john/Listing/Listing.java
@@ -6,16 +6,19 @@
 
 package com.papa247.john.Listing;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import com.papa247.john.DataBases;
 import com.papa247.john.Enumerators.Amminities;
 import com.papa247.john.Enumerators.ListingType;
+import com.papa247.john.Support.Review;
 import com.papa247.john.User.User;
 
 public class Listing {
 
     public Address address;
-    public int id;
-    public String appartmentNumber;
+    public String id;
+    public String apartmentNumber;
     public double monthlyPrice;
     public double rentLength;
     public User owner;
@@ -24,7 +27,40 @@ public class Listing {
     public Room[] bedrooms;
     public Amminities[] amminities;
     public ListingType listingType;
+    public String userName;
+    public Listing(JSONObject listing) {
+        id = (String)listing.get("id");
+        apartmentNumber = (String)listing.get("apartmentNumber");
+        monthlyPrice = (double)listing.get("montlyPrice");
+        rentLength = (double)listing.get("rentLength");
     
+        userName = listing.get("owner");
+        owner = DataBases.getUser(userName,username);
+        
+                
+    }
+    public void addRoom(Room room) {
+        for(int i = 0;i<rooms.length;i++) {
+            if(rooms[i]==null) {
+                rooms[i] = room;
+            }
+        }
+    }
+    public void removeRoom(Room room) {
+        for(int i = 0;i<rooms.length;i++) {
+            if(rooms[i]!=null&&rooms[i].equals(room)) {
+                rooms[i]=null;            
+            }
+        }
+    }
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        totalPrice = rentLength*=montlyPrice;
+        return totalPrice;
+    }
+    public double getRentLength() {
+        return rentLength;
+    }
     
     public double getTotalSize() {
         double size = 0;
@@ -32,6 +68,24 @@ public class Listing {
             size += room.size;
         return size;
     }
-
-    
+    public JSONObject toJSON() {
+        JSONObject jo = new JSONObject();
+        
+        jo.put("id", id);
+        jo.put("apartmentNumber", apartmentNumber);
+        jo.put("monthlyPrice", monthlyPrice);
+        jo.put("rentLength", rentLength);
+        jo.put("owner", userName);
+        JSONArray ja = new JSONArray();
+        for (Amminities amminities : amminities)
+            ja.put(amminities);
+        jo.put("amminities", ja);   
+        ja = new JSONArray();
+        for (ListingType listingType : listingType)
+            ja.put(listingType);
+        jo.put("listingType", ja);
+        return jo;    }
+    public boolean Delete() {
+        return false;
+    }
 }
